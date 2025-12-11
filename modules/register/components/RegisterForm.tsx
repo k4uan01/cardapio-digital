0ade9e4fe069681e1ab0c4f7e17018be44451926
Kauan <kauanchefen@gmail.com>
2025-12-10 21:02:43 -0300

    fix: reduz tamanho do logo do Google nos formulÃ¡rios de login e registro

--- modules/register/components/RegisterForm.tsx
+++ modules/register/components/RegisterForm.tsx
@@ -127,7 +127,7 @@ export function RegisterForm() {
             disabled={isLoading || isLoadingGoogle}
           >
             <svg
-              className="mr-2 h-5 w-5"
+              className="mr-2 h-4 w-4 flex-shrink-0"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
